bcbc35a0b9bddee1e5e1768edbf2c82ad99f314e
ogy <geunyoung.oh@oracle.com>
2018-09-17 18:02:29 +0900

    at p.m.6:00 on 17th Sep.

---
+++ JAVA OP Tutorial/src/OP_Tutorial/console.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+/**
+ * @author GEUNYOUNG OH
+ *
+ */
+package OP_Tutorial;
+
+public class console {
+	public static void main(String[] args) {
+		System.out.println("¾È³ç");
+		System.out.println("Helloworld");
+	}
+}
