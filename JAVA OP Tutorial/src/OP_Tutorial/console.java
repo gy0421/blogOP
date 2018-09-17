8340bf019ef55ab376d5d2dc40f647fecc4decf0
ogy <geunyoung.oh@oracle.com>
2018-09-17 18:09:09 +0900

    인코딩 utf-8로 변경

--- JAVA OP Tutorial/src/OP_Tutorial/console.java
+++ JAVA OP Tutorial/src/OP_Tutorial/console.java
@@ -9,7 +9,7 @@ package OP_Tutorial;
 
 public class console {
 	public static void main(String[] args) {
-		System.out.println("유티에프팔");
+		System.out.println("유티에프팔...");
 		System.out.println("Helloworld");
 	}
 }
